949b76497058225bc9c9634ce63054a8087d5010
Min Lwin <m.lwin@utexas.edu>
2015-12-22 00:29:31 -0600

    initial attempt at notebook

---
+++ main.py
@@ -0,0 +1,41 @@
+####################################################################################
+6-handed Euchre
+####################################################################################
+all possible teams play all other possible teams
+
+13 total players
+
+13 choose 3 = 286 combinations, ie, 286 ways to select 3 players from the set of 
+13 players where order of selection does not matter. 
+
+In other words, there are a total of 286 possible 3 person teams.  
+
+For each possible team (of the 286), if we desire they play every other team, there are a
+total of 286 * 286 = 81796 total possible games with distinct teams.
+
+####################################################################################
+####################################################################################
+
+####################################################################################
+4-handed Euchre
+####################################################################################
+all possible teams play all other possible teams
+
+13 total players
+
+13 choose 2 = 78 combinations, ie, 78 ways to select 2 players from the set of 
+13 players where order of selection does not matter. 
+
+In other words, there are a total of 78 possible 2 person teams.  
+
+For each possible team (of the 78), if we desire they play every other team, there are a
+total of 78 * 78 = 6084 total possible games with distinct teams.
+
+Each game comprises of 2 teams.  With 3 tables of 4-handed euchre, the number of rounds 
+required to play all 6084 games is 6084/3/2 = 507 rounds.
+
+####################################################################################
+####################################################################################
+
+
+
